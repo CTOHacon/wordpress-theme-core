7bb2d585e797fa7da5a3e9a362eabd32539a0e66
AlexanderBorysenko <borysenko.alexander@gmail.com>
2025-05-06 07:12:21 +0300

    refactor: Optimize frontend script for setting CSS variable on body width change

--- src/ThemeModules/BodyWidthCssComputedVariable/BodyWidthCssComputedVariable.php
+++ src/ThemeModules/BodyWidthCssComputedVariable/BodyWidthCssComputedVariable.php
@@ -16,19 +16,32 @@ class BodyWidthCssComputedVariable extends ThemeModule
     {
         ob_start();
         ?>
-        <script>
-            document.addEventListener('DOMContentLoaded', function ()
+        <script>(function ()
             {
-                document.documentElement.style.setProperty(
-                    "--<?= $this->cssVariableName ?>",
-                    `${document.body.clientWidth}px`
-                )
-            });
-            window.addEventListener('resize', () =>
-            {
-                document.documentElement.style.setProperty('--<?= $this->cssVariableName ?>', `${document.body.clientWidth}px`);
-            });
-        </script>
+                var setVar = function ()
+                {
+                    if (document.body)
+                    {
+                        document.documentElement.style.setProperty('--<?= $this->cssVariableName ?>', document.body.clientWidth + 'px');
+                    }
+                };
+                if (document.body)
+                {
+                    setVar();
+                } else
+                {
+                    var observer = new MutationObserver(function ()
+                    {
+                        if (document.body)
+                        {
+                            setVar();
+                            observer.disconnect();
+                        }
+                    });
+                    observer.observe(document.documentElement, { childList: true, subtree: true });
+                }
+                window.addEventListener('resize', setVar, { passive: true });
+            })();</script>
         <?php
         return ob_get_clean();
     }
