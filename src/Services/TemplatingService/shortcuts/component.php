1367cf87474f6c66108da0a993ed9ea3da17d306
AlexanderBorysenko <borysenko.alexander@gmail.com>
2026-03-06 01:43:39 +0100

    fix: deprecate component() function in favor of render_component_template()

--- src/Services/TemplatingService/shortcuts/component.php
+++ src/Services/TemplatingService/shortcuts/component.php
@@ -2,9 +2,20 @@
 
 use Hacon\ThemeCore\Services\TemplatingService\ComponentRenderService;
 
-function component(string $component, array $htmlAttributes = [], array $props = [])
-{
+if (!function_exists('component')) {
+    /**
+     * @deprecated Use render_component_template() instead
+     */
+    function component(string $component, array $htmlAttributes = [], array $props = [])
+    {
+        $processor = new ComponentRenderService($component, $htmlAttributes, $props);
+        $processor->render();
+    }
 
+}
+
+function render_component_template(string $component, array $htmlAttributes = [], array $props = [])
+{
     $processor = new ComponentRenderService($component, $htmlAttributes, $props);
     $processor->render();
 }
